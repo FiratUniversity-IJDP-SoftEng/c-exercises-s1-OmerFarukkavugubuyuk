bf020b5d6f4725eed7fb87991043b2b6aeca6839
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-10-25 22:48:32 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+void sayHello(char name[])//printing function
+   {
+       printf("hello %s", name);//printing 
+       
+   }
+   
+int main()
+{
+    char name[100];
+    printf("Please enter your name : ");
+    scanf("%99s",name);//getting name
+    sayHello(name);//running function
+   
+    return 0;
+}
