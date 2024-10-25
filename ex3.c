09df54995c288830890f81b5195a2e64785e11ab
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-10-25 22:25:28 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   char answer[5];  //answer of the question
+    
+    printf("Do you have a cat or a dog? ");//question
+    scanf("%5s", answer);  //new answer
+
+    if(strcmp(answer, "cat") == 0)//comparing
+    {
+        printf("meow");
+        
+    }else if(strcmp(answer, "dog") == 0)//comparing
+    {
+        printf("hav");
+        
+    }
+
+    return 0;
+}
 
