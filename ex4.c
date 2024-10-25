c199d56f54d70e4dec910604fc5967f58e3807fa
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-10-25 22:37:19 +0300

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int num;//number that we will print and use as a counter
+    for(num=2;num<=10;num=num+2){//loop
+        
+        printf("%d\n",num);//printing numbers
+        
+    }
+    
+    return 0;
+}
