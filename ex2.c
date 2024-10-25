78625df87f82a4b54e02a46ce52fa1be5e0ed105
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-10-25 21:48:21 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main()
+{
+    int age;//users age
+    int i;//counter
+    printf("Please enter your age : ");
+    scanf("%d",&age);
+    
+    for(i=1; i <=age; i++)
+    {
+        printf("Happy Birth Day \n");
+    }
+    return 0;
+}
