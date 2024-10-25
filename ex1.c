f69d1885deb5b02ab13e10ec2b34a33a66e214de
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-10-25 22:26:58 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main()
+{
+    int sayi;
+
+    printf("Guess a number between 1 and 5 : ");
+    scanf("%d", &sayi);
+
+    if (sayi == 3)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+    return 0;
+}
